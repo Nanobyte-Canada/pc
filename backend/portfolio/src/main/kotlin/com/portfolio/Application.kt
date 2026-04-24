1154db4cdac79fbc937defca8480fbed10c004a3
Saurabh Bilakhia Dawson <sbilakhia@dawsonpartners.com>
2026-04-23 22:44:58 -0400

    fix(portfolio): register BrokerGatewayConfig in EnableConfigurationProperties

--- backend/portfolio/src/main/kotlin/com/portfolio/Application.kt
+++ backend/portfolio/src/main/kotlin/com/portfolio/Application.kt
@@ -4,12 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import com.portfolio.broker.client.BrokerGatewayConfig
 import com.portfolio.broker.config.BrokerEncryptionConfig
 import com.portfolio.broker.config.BrokerSyncConfig
 
 @SpringBootApplication
 @EnableScheduling
-@EnableConfigurationProperties(BrokerEncryptionConfig::class, BrokerSyncConfig::class)
+@EnableConfigurationProperties(BrokerEncryptionConfig::class, BrokerSyncConfig::class, BrokerGatewayConfig::class)
 class Application
 
 fun main(args: Array<String>) {
